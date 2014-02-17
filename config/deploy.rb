eca884b0721cdbfb8293d1f80c70f762e1b3b77e
Fidel Oyarzo <fidel.oyarzo@gmail.com>
2014-02-17 12:30:45 -0300

    uniocprm

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,30 +35,6 @@ after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
 after 'deploy:restart', 'unicorn:restart'   # app preloaded
 after 'deploy:restart', 'unicorn:duplicate'
 
-# Unicorn
-namespace :unicorn do
-  def run_unicorn
-    "cd #{current_path} ; RAILS_ENV=#{rails_env} bundle exec unicorn_rails -c config/unicorn.rb -D"
-  end
-
-  desc "Zero-downtime restart of Unicorn"
-  task :restart, except: { no_release: true } do
-    cmd = "if [ -f #{unicorn_pid} ]; then kill -s USR2 `cat #{unicorn_pid}`; else #{run_unicorn} ; fi"
-    run cmd
-  end
-
-  desc "Start unicorn"
-  task :start, except: { no_release: true } do
-    run run_unicorn
-  end
-
-  desc "Stop unicorn"
-  task :stop, except: { no_release: true } do
-    run "if [ -f #{unicorn_pid} ]; then kill -s QUIT `cat #{unicorn_pid}` ; fi"
-  end
-end
-
-
 # Assets
 namespace :assets do
   desc "Precompile assets locally and then rsync to app servers"
