704c88ca3b824318897b1b93ce17e26d4e2b60ea
Fidel Oyarzo <fidel.oyarzo@gmail.com>
2014-02-17 13:34:35 -0300

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,32 @@
 # setup multistage
 set :stages, %w(staging production)
 set :default_stage, 'staging'
+require 'capistrano/ext/multistage'
+require 'capistrano-unicorn'
 set :application, 'ZUAGRO'
+
+
 #server
+set :domain, 'zuagro.com'
 set :user, 'zuagro'
-set :deploy_to, '/var/www/zuagro'
 set :use_sudo, false
+set :deploy_to, '/var/www/zuagro'
+
+#local
+set :local_repository,  '~/Documents/vagrant/ruby/zuagro/.git'
+
 #git
 set :repository,  'git@github.com:FiDeLio/zuagro.git'
 set :branch, 'master'
 set :scm, :git
 set :repository_cache, 'git_cache'
 set :deploy_via, :remote_cache
-set :ssh_options, { :forward_agent => true }
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
+
+
+after 'deploy', 'deploy:bundle_gems'
+after 'deploy:bundle_gems', 'deploy:restart'
+after 'deploy:update_code', 'deploy:migrate'
+after 'deploy', 'deploy:cleanup'
