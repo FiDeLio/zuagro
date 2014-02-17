a99ae270f6e0ea434c1056262af781e0351c9e2f
Fidel Oyarzo <fidel.oyarzo@gmail.com>
2014-02-17 12:49:36 -0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,21 +20,7 @@ set :ssh_options, { :forward_agent => true }
 
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
-after "deploy:create_symlink", "assets:precompile"
-after 'deploy:restart', 'unicorn:reload'    # app IS NOT preloaded
-after 'deploy:restart', 'unicorn:restart'   # app preloaded
+after 'deploy:create_symlink', 'assets:precompile'
+after 'deploy:restart', 'unicorn:reload'
+after 'deploy:restart', 'unicorn:restart'
 after 'deploy:restart', 'unicorn:duplicate'
-
-# Assets
-namespace :assets do
-  desc "Precompile assets locally and then rsync to app servers"
-  task :precompile, only: { :primary => true } do
-    run_locally "mkdir -p public/__assets; mv public/__assets public/assets;"
-    run_locally "bundle exec rake assets:precompile;"
-    servers = find_servers roles: [:app], except: { :no_release => true }
-    servers.each do |server|
-      run_locally "rsync  -rave  'ssh -i /Users/fidel/.ssh/zuagro' ./public/assets/ #{user}@#{server}:#{current_path}/public/assets/;"
-    end
-    run_locally "mv public/assets public/__assets"
-  end
-end
