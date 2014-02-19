a6ef1758be78ef6ddc9c57909022727993dec1ac
Fidel Oyarzo <fidel.oyarzo@gmail.com>
2014-02-19 12:31:49 -0300

    precompile false

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,9 +24,9 @@ ssh_options[:forward_agent] = true
 ssh_options[:keys] = ['~/.ssh/zuagro']
 
 after "deploy:restart", "unicorn:restart"
-after "deploy:create_symlink", "assets:precompile"
+#after "deploy:create_symlink", "assets:precompile"
 
-before  'deploy:assets:precompile', 'deploy:migrate'
+#before  'deploy:assets:precompile', 'deploy:migrate'
 
 # Unicorn
 namespace :unicorn do
@@ -48,20 +48,5 @@ namespace :unicorn do
   desc "Stop unicorn"
   task :stop, except: { no_release: true } do
     run "if [ -f #{unicorn_pid} ]; then kill -s QUIT `cat #{unicorn_pid}` ; fi"
-  end
-end
-
-
-# Assets
-namespace :assets do
-  desc "Precompile assets locally and then rsync to app servers"
-  task :precompile, only: { :primary => true } do
-    run_locally "mkdir -p public/__assets; mv public/__assets public/assets;"
-    run_locally "bundle exec rake assets:precompile;"
-    servers = find_servers roles: [:app], except: { :no_release => true }
-    servers.each do |server|
-      run_locally "rsync  -rave 'ssh -i /Users/fidel/.ssh/zuagro'   ./public/assets/ #{user}@#{server}:#{current_path}/public/assets/;"
-    end
-    run_locally "mv public/assets public/__assets"
   end
 end
